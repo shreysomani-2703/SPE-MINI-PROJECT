48441acf8c7e3bda8c8922e4333b921356186759
shreysomani-2703 <sdsomani27@gmail.com>
2026-03-05 15:28:11 +0530

    converted calculator CLI to flask API

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-from calculator import *
 
-while True:
-    print("\nScientific Calculator")
-    print("1. Square Root")
-    print("2. Factorial")
-    print("3. Natural Log")
-    print("4. Power")
-    print("5. Exit")
+from flask import Flask, request
+import math
 
-    choice = int(input("Enter choice: "))
+app = Flask(__name__)
 
-    if choice == 1:
-        x = float(input("Enter number: "))
-        print("Result:", square_root(x))
+@app.route("/")
+def home():
+    return "Scientific Calculator API Running"
 
-    elif choice == 2:
-        x = int(input("Enter number: "))
-        print("Result:", factorial(x))
+@app.route("/add")
+def add():
+    a = float(request.args.get("a"))
+    b = float(request.args.get("b"))
+    return {"result": a + b}
 
-    elif choice == 3:
-        x = float(input("Enter number: "))
-        print("Result:", natural_log(x))
+@app.route("/sub")
+def sub():
+    a = float(request.args.get("a"))
+    b = float(request.args.get("b"))
+    return {"result": a - b}
 
-    elif choice == 4:
-        x = float(input("Enter base: "))
-        b = float(input("Enter exponent: "))
-        print("Result:", power(x, b))
+@app.route("/mul")
+def mul():
+    a = float(request.args.get("a"))
+    b = float(request.args.get("b"))
+    return {"result": a * b}
 
-    elif choice == 5:
-        break
+@app.route("/div")
+def div():
+    a = float(request.args.get("a"))
+    b = float(request.args.get("b"))
+    return {"result": a / b}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
